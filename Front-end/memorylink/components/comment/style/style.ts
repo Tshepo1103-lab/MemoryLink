eeb9e1bbc6a06c2a008ddd469b786a75527c9b51
Tshepo-Mahlangu-boxfusion <159532803+Tshepo-Mahlangu-boxfusion@users.noreply.github.com>
2024-04-28 18:55:00 +0200

    added ui

---
+++ Front-end/memorylink/components/comment/style/style.ts
@@ -0,0 +1,12 @@
+import { createStyles,css} from "antd-style";
+
+export const useStyles=createStyles({
+    main: css`
+    height: 100px;
+    width:100%;
+  `,
+    input:css`
+        width:100%;
+        height:10vh;
+    `
+})
